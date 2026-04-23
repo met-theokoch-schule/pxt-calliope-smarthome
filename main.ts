5ccb538fae634c82c36cbdf9a085c55ea24e699b
met-theokoch-schule <91288462+met-theokoch-schule@users.noreply.github.com>
2026-04-23 22:22:49 +0200

    empty starting blocks

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-smarthome.onTouchSensorPressed(smarthome.TouchSwitch.S7, smarthome.TouchPressType.Short, function () {
-    smarthome.setWallLampLedColor(1, 0xff00ff)
-})
 basic.forever(function () {
 	
 })
